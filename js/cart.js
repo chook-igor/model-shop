ac9c5acf6b0deb471d6abb74f82d10a40f8bf4d8
chook-igor <igor_chuyko@hotmail.com>
2020-06-15 18:27:06 +0300

    On final

--- js/cart.js
+++ js/cart.js
@@ -1,18 +1,80 @@
-const cartContainer = document.querySelector('.ordered-items')
-let ordered = {...localStorage};
-let orderPos = '';
-let key = '';
-let productList = '';
+const cartContainer = document.querySelector(".ordered-items");
+
 function loadOrder() {
-  for (key in ordered) {
-    orderPos = ordered[key].split(',');
-    productList += `<div class="product"> 
-                      <p class="product-name">${orderPos[0]}</p>
-                      <p class="product-price">${orderPos[1]} $</p>
-                      <hr>
-                      </div>`;
+  let qnty = 1;
+  let productList = "";
+  for (let i = 0; i < localStorage.length; i++) {
+    key = localStorage.key(i);
+    orderPos = JSON.parse(localStorage.getItem(key));
+    productList += `<div class="product" id="id${orderPos.id}"> 
+                        <div>
+                          <p class="product-name">${orderPos.item}</p>
+                        </div>
+                        <div>
+                          <p class="product-price${
+                            orderPos.id
+                          }">${orderPos.price.toFixed(2)}</p>
+                        </div>
+                        <div>
+                          <p class="item-add" data-id="${
+                            orderPos.id
+                          }" style="text-align:center">&#x2295</p>
+                        </div>
+                        <div>
+                          <p class="qnty${
+                            orderPos.id
+                          }" style="text-align:center" >${qnty}</p>
+                        </div>
+                        <div>
+                          <p class="item-rem" data-id="${
+                            orderPos.id
+                          }" style="text-align:center">&#x2296</p>
+                        </div>
+                        <div>
+                          <p class="item-total${orderPos.id}" id="item-total">${
+      orderPos.price * qnty
+    }</p>
+                        </div>
+                      </div><hr>`;
     cartContainer.innerHTML = productList;
-    }
+  }
 }
 
 loadOrder();
+
+document
+  .querySelectorAll(".item-add")
+  .forEach((plus) => plus.addEventListener("click", increase));
+
+function increase() {
+  const plus = event.target;
+  let qntyId = plus.dataset.id;
+  let qnty = parseInt(document.querySelector(`.qnty${qntyId}`).innerHTML);
+  let itemPrice = parseInt(
+    document.querySelector(`.product-price${qntyId}`).innerHTML
+  );
+  document.querySelector(`.qnty${qntyId}`).innerHTML = qnty += 1;
+  document.querySelector(`.item-total${qntyId}`).innerHTML = qnty * itemPrice;
+}
+
+document
+  .querySelectorAll(".item-rem")
+  .forEach((minus) => minus.addEventListener("click", decrease));
+
+function decrease() {
+  const plus = event.target;
+  let qntyId = plus.dataset.id;
+  let qnty = document.querySelector(`.qnty${qntyId}`).innerHTML;
+  let itemPrice = parseInt(
+    document.querySelector(`.product-price${qntyId}`).innerHTML
+  );
+
+  document.querySelector(`.qnty${qntyId}`).innerHTML = qnty -= 1;
+  document.querySelector(`.item-total${qntyId}`).innerHTML = qnty * itemPrice;
+  if (qnty < 1) document.querySelector(`#id${qntyId}`).innerHTML = "";
+}
+
+document.querySelector(".delivery-info").addEventListener("click", send);
+function send() {
+  localStorage.clear();
+}
--- js/index.js
+++ js/index.js
@@ -1,48 +1,56 @@
-const container = document.querySelector('.shop');
-const cartIndex = document.querySelector('.ordered-qty')
+const container = document.querySelector(".shop");
+const cartIndex = document.querySelector(".ordered-qty");
 let products = [];
 let order = [];
 
 async function getProducts() {
-  const response = await fetch('products.json')
-  products =  await response.json();
+  const response = await fetch("products.json");
+  products = await response.json();
   showProducts();
 }
 
 function showProducts(category) {
-  let productList = '';
+  let productList = "";
   products
-  .filter( product => category ? product.category === category : true )
-  .forEach(product => {
-    productList += `<div class="product"> 
-    <img class="product-image" src="img/product/${product.image}" alt="${product.item}">
-    <p class="product-name">${product.item}</p>
-    <p class="product-price">${product.price.toFixed(2)} $</p>
-    <button class="button" data-id=${product.id} >Add to cart</button>
-    </div>`;
-  });
+    .filter((product) => (category ? product.category === category : true))
+    .forEach((product) => {
+      productList += `<div class="product"> 
+      <img class="product-image" src="img/product/${product.image}" alt="${
+        product.item
+      }">
+      <p class="product-name">${product.item}</p>
+      <p class="product-price">${product.price.toFixed(2)} $</p>
+      <button class="button" data-id="${product.id}" >Add to cart</button>
+      </div>`;
+    });
   container.innerHTML = productList;
-  
-  document.querySelectorAll('.product button')
-  .forEach((butn) => butn.addEventListener("click", addToCart));
+
+  document
+    .querySelectorAll(".product button")
+    .forEach((butn) => butn.addEventListener("click", addToCart));
   function addToCart() {
     const button = event.target;
     const id = button.dataset.id;
-    order = [products[id-1].item, products[id-1].price.toFixed(2)];
+
+    order = JSON.stringify({
+      id: products[id - 1].id,
+      item: products[id - 1].item,
+      price: products[id - 1].price,
+    });
     localStorage.setItem(id, order);
     cartIndex.innerHTML = localStorage.length;
   }
 }
+
 getProducts();
 
-document.querySelector('.all').addEventListener('click', () => showProducts());
-document.querySelector('.big').addEventListener('click', () => showProducts('32'));
-document.querySelector('.med').addEventListener('click', () => showProducts('48'));
-document.querySelector('.sf').addEventListener('click', () => showProducts('sf'));
-
-  
-
-
-
-
-   
+document.querySelector(".all").addEventListener("click", () => showProducts());
+document
+  .querySelector(".big")
+  .addEventListener("click", () => showProducts("32"));
+document
+  .querySelector(".med")
+  .addEventListener("click", () => showProducts("48"));
+document
+  .querySelector(".sf")
+  .addEventListener("click", () => showProducts("sf"));
